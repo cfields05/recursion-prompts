401027cfa54634b0fb8773f7dfaa7a55b37d3d90
OP-Bright <123033559+OP-Bright@users.noreply.github.com>
2024-10-04 20:51:36 +0000

    you really want sum o dis.

--- src/recursion.js
+++ src/recursion.js
@@ -33,7 +33,18 @@ var sum = function(array, result = 0) {
 
 // 3. Sum all numbers in an array containing nested arrays.
 // Example: arraySum([1,[2,3],[[4]],5]); // 15
-var arraySum = function(array) {
+var arraySum = function(array, result = 0) {
+  //base - if array is empty
+  if (!array.length) {
+    return result;
+  }
+  // recurse
+  if (Array.isArray(array[0])) {
+    result += arraySum(array[0])
+  } else {
+    result += array[0];
+  }
+  return arraySum(array.slice(1, array.length), result);
 };
 
 // 4. Check if a number is even.
